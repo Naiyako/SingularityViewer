5d45ac1d4bbaef9ad6ceac14968ecaf5767a60ac
Naiyako <Lys>
2019-07-03 22:30:43 +0200

    Make the Viewer more stable again. Check. Adding a bit more 64bit, because why not. Detect available VRam and set the max Texture-Cache to available VRam -1024MB for safety reason and other programs want something too.

--- indra/newview/llviewertexturelist.cpp
+++ indra/newview/llviewertexturelist.cpp
@@ -1284,10 +1284,9 @@ LLPointer<LLImageJ2C> LLViewerTextureList::convertToUploadFile(LLPointer<LLImage
 // Returns min setting for TextureMemory (in MB)
 S32Megabytes LLViewerTextureList::getMinVideoRamSetting()
 {
-	S32Megabytes system_ram = gSysMemory.getPhysicalMemoryKB();
-	LL_INFOS() << system_ram << LL_ENDL;
+	U32Megabytes system_ram = gSysMemory.getPhysicalMemoryKB();
 	//min texture mem sets to 64M if total physical mem is more than 1.5GB
-	return (system_ram > S32Megabytes(1500)) ? S32Megabytes(64) : gMinVideoRam ;
+	return (system_ram > U32Megabytes(1500)) ? S32Megabytes(64) : gMinVideoRam ;
 }
 
 //static
